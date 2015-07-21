fa8ade901309fa8764c159e6f67ccdb5851cff50
edwardpmorris <edward.morris@uca.es>
2015-07-21 15:30:03 +0200

    added some definitions

---
+++ vignettes/unitlabels_vignette.R
@@ -0,0 +1,22 @@
+## ---- results='asis'-----------------------------------------------------
+# load the package
+library(unitlabels)
+
+## ---- results='asis'-----------------------------------------------------
+# return Rmarkdown label for temperature in degrees Celcius
+ul("T.degC", "md")
+
+## ------------------------------------------------------------------------
+plot(1:10,1:10, ylab = ul("T.degC", "R"), xlab = ul("Tsw.degC", "R"))
+
+## ---- results='asis'-----------------------------------------------------
+system.file("extdata", package = "unitlabels")
+
+## ---- results='asis'-----------------------------------------------------
+require(unitlabels)
+def <- system.file("extdata", package = "unitlabels")
+def <- paste0(def,"/definitions.csv")
+def <- read.csv(def, as.is = T)
+knitr::kable(def[,c("standard.name", "sh.symbol", "symbol.md")])
+knitr::kable(def[,c("standard.name", "sh.units", "units.md")])
+
